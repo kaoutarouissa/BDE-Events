920d1f1eaaf6baae24f8171eb60c7ab2b82364d7
kaoutar <hodabz091@gmail.com~>
2026-07-22 13:29:36 +0100

    Ajout de la clé étrangère user_id dans la table reservations

--- database/migrations/2026_07_22_122742_add_user_id_to_reservations_table.php
+++ database/migrations/2026_07_22_122742_add_user_id_to_reservations_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::table('reservations', function (Blueprint $table) {
             //
+              $table->foreignId('user_id')
+              ->constrained()
+              ->onDelete('cascade');
         });
     }
 
