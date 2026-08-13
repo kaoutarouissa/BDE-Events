3616e1d462646b01fcb0f355c5e1878be9c479fb
kaoutar <you@example.com>
2026-08-13 02:11:36 +0100

    regrouper partier react et ajouter dockerfile et nginx

--- bde-events-api/app/Http/Controllers/LoginController.php
+++ bde-events-api/app/Http/Controllers/LoginController.php
@@ -8,31 +8,27 @@ use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     //
-    public function login(Request $request){
-    $validate=$request->validate([
-        'email'=>'required|email',
-        'password'=>'required'
+   public function login(Request $request)
+{
+    $credentials = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
     ]);
-      if (Auth::attempt($validate)) {
-          $request->session()->regenerate();
-          $user=Auth::user();
 
-        if ($user->role == 'bde') {
-            return redirect()->route('bde-dashboard');
-        }
-        
-         if ($user->role == 'etudiant') {
-            return redirect()->route('etudiant-dashboard');
-        }
-        }
-                  $user=Auth::user();
-
-            $token = $user->createToken('auth_token')->plainTextToken;
-
-      return response()->json([
-        'message' => 'Connexion réussie',
-        'user' => $user,
-        'token' => $token
-    ], 200);
+    if (!Auth::attempt($credentials)) {
+        return response()->json([
+            'message' => 'Email ou mot de passe incorrect'
+        ], 401);
     }
+
+    $user = Auth::user();
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+        'message' => 'Connexion réussie',
+        'token' => $token,
+        'user' => $user,
+    ], 200);
+}
 }
