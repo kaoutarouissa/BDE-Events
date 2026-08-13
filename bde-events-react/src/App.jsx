43d1f10fc2017aa28247201295234b66e48cac3f
kaoutar <you@example.com>
2026-08-13 02:29:37 +0100

    Ajout de la dockerisation de la SPA React

--- bde-events-react/src/App.jsx
+++ bde-events-react/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Register from "./pages/Register";
 import BdeDashboard from "./pages/BdeDashboard";
 import EtudiantDashboard from "./pages/EtudiantDashboard";
